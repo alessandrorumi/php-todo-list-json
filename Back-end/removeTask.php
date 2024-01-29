00521497208160b03e569583fec9051d6172d560
alessandrorumi <rumi.alessandro21@gmail.com>
2024-01-29 09:44:13 +0100

    Fix: Remove id task

--- Back-end/removeTask.php
+++ Back-end/removeTask.php
@@ -4,11 +4,11 @@
   header("Access-Control-Allow-Headers: X-Requested-With");
   header("Access-Control-Allow-Origin: http://localhost:5173");
 
-  $index = $_GET['index'];
+  $id = $_GET['id'];
   $jsonTasks = file_get_contents('tasks.json');
   $tasks = json_decode($jsonTasks, true);
 
-  array_splice($tasks, $index, 1);
+  array_splice($tasks, $id, 1);
 
   $jsonTasks = json_encode($tasks);
   file_put_contents('tasks.json', $jsonTasks);
